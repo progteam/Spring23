34fbb1c232ba4a480ce62304186d11a0e6235a91
DominicS99 <98355617+DominicS99@users.noreply.github.com>
2023-03-12 16:41:02 -0700

    Create dominic.cpp

---
+++ Week 1/Contest (Mar 11)/Proofs/dominic.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+inline vector<string> split(const string& s) {
+  stringstream ss(s);
+  vector<string> res;
+  for (string w; ss >> w;) res.push_back(w);
+  return res;
+}
+
+int main() {
+  ios::sync_with_stdio(0); cin.tie(NULL);
+  
+  int n;
+  string s;
+  cin >> n;
+  cin.ignore();
+
+  unordered_set<string> dict;
+  for (int i = 1; i <= n; i++) {
+    getline(cin, s);
+    vector<string> line = split(s);
+    bool arrow = false;
+    for (string &curr : line) {
+      if (curr == "->") {
+        arrow = true;
+        continue;
+      }
+
+      if (arrow) {
+        dict.insert(curr);
+      } else if (!dict.count(curr)) {
+        cout << i << endl;
+        return 0;
+      }
+    }
+  }
+
+  cout << "correct" << endl;
+  return 0;
+}

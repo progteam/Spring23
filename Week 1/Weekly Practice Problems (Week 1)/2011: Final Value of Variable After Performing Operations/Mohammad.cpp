ff92f7614480e0ac0202a6c03a3d776c736945dd
haroon73 <112339141+haroon73@users.noreply.github.com>
2023-03-17 14:44:08 -0700

    Mohammad.cpp

---
+++ Week 1/Weekly Practice Problems (Week 1)/2011: Final Value of Variable After Performing Operations/Mohammad.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        
+     int x = 0; 
+     for(string i : operations){
+         if(i == "++X" || i == "X++"){
+             x++;
+         }
+         if(i == "--X" || i == "X--"){
+              x--;
+         }
+       
+            
+         }
+
+     return x;
+     }
+
+   
+
+    
+};

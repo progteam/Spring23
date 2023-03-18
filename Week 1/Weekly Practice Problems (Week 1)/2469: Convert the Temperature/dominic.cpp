f2960ff0d5497cdb4b46bb846b11289ec5be04ba
DominicS99 <98355617+DominicS99@users.noreply.github.com>
2023-03-17 21:18:49 -0700

    Update Week 1/Weekly Practice Problems (Week 1)/2469: Convert the Temperature/dominic.cpp
    Co-authored-by: Donald Dong <ddong@alumni.csumb.edu>

--- Week 1/Weekly Practice Problems (Week 1)/2469: Convert the Temperature/dominic.cpp
+++ Week 1/Weekly Practice Problems (Week 1)/2469: Convert the Temperature/dominic.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     vector<double> convertTemperature(double celsius) {
-        vector<double> res = {celsius+273.15, celsius*1.8 + 32};
-        return res;
+        return {celsius+273.15, celsius*1.8 + 32};
     }
 };

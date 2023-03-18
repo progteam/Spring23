20af6024910e358424ccc3b2605aed8752a2b9f4
DominicS99 <98355617+DominicS99@users.noreply.github.com>
2023-03-17 21:14:11 -0700

    Update Week 1/Weekly Practice Problems (Week 1)/2011: Final Value of Variable After Performing Operations/dominic.cpp
    Co-authored-by: Donald Dong <ddong@alumni.csumb.edu>

--- Week 1/Weekly Practice Problems (Week 1)/2011: Final Value of Variable After Performing Operations/dominic.cpp
+++ Week 1/Weekly Practice Problems (Week 1)/2011: Final Value of Variable After Performing Operations/dominic.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int finalValueAfterOperations(vector<string>& operations) {
         int x = 0;
-        for (string op : operations) {
+        for (string& op : operations) {
             op[1] == '+' ? x++ : x--;
         }
         return x;

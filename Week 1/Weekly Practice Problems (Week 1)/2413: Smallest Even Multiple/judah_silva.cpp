898e156833fcc542ac47c871f46a0c6bac60cafa
Judah-Silva <123522885+Judah-Silva@users.noreply.github.com>
2023-03-17 20:41:10 -0700

    Create judah_silva.cpp (#37)

---
+++ Week 1/Weekly Practice Problems (Week 1)/2413: Smallest Even Multiple/judah_silva.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int smallestEvenMultiple(int n) {
+        for (int i = n; ; i += n) {
+            if ((i % 2) == 0) return i;
+        }
+        return -1;
+    }
+};

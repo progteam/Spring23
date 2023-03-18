dbfe5de90570a9378e39461ef0383b693cb80679
DominicS99 <98355617+DominicS99@users.noreply.github.com>
2023-03-17 19:33:01 -0700

    Apply suggestions from code review

--- Week 1/Weekly Practice Problems (Week 1)/0709: To Lower Case/judah_silva.cpp
+++ Week 1/Weekly Practice Problems (Week 1)/0709: To Lower Case/judah_silva.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     string toLowerCase(string s) {
         for (int i = 0; i < s.length(); i++) {
-            if (s[i] >= 65 && s[i] <= 90) {
-                s[i] += 32;
+            if (s[i] >= 'A' && s[i] <= 'Z') {
+                s[i] += 'a' - 'A';
             }
         }
         return s;

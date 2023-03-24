1880283a1826aa573d2ad51c4b94fd225397473d
An. Patil <123909570+mimzowich@users.noreply.github.com>
2023-03-24 15:53:31 -0700

    Update anshu.py

--- Week 2/Contest (Mar 20)/Sun and Moon/anshu.py
+++ Week 2/Contest (Mar 20)/Sun and Moon/anshu.py
@@ -2,13 +2,12 @@ s1,s2 = map(int, input().split())
 m1,m2 = map(int, input().split())
 
 
-def eclipse(s1, s2, m1, m2):
-    year = 1
-    while True:
-        sun_align = (s1 + year) % s2 == 0
-        moon_align = (m1 + year) % m2 == 0
-        if sun_align and moon_align:
-            print(year)
-            return year
-        year += 1
-eclipse(s1, s2, m1, m2) 
+
+year = 1
+while True:
+    sun_align = (s1 + year) % s2 == 0
+    moon_align = (m1 + year) % m2 == 0
+    if sun_align and moon_align:
+        print(year)
+        break
+    year += 1

f350815aaee7ab43a26558117ac4301c192f993d
Delight Lee <98308370+delightgit@users.noreply.github.com>
2023-04-21 14:51:43 -0700

    Update delight2595.java
    Tried this problem again with bitwise shift

--- Week 2/Weekly Practice Problems (Week 2)/2595: Number of Even and Odd Bits/delight2595.java
+++ Week 2/Weekly Practice Problems (Week 2)/2595: Number of Even and Odd Bits/delight2595.java
@@ -1,4 +1,25 @@
+/*Solution using bitwise shift*/
+class Solution {
+    public int[] evenOddBit(int n) {
+        int num = n;
+        int even = 0, odd = 0;
+        for(int i = 0; num > 0; i++){
+            if((num & 1) == 1){
+                if(i % 2 == 0){
+                    even++;
+                }else{
+                    odd++;
+                }
+            }
+            num = num >>> 1;
+        }
+        int[] answer = {even, odd};
+        return answer;
+    }
+}
+
 /*Solution using int*/
+/*
 class Solution {
     public int[] evenOddBit(int n) {
         int num = n;
@@ -17,7 +38,9 @@ class Solution {
         return answer;
     }
 }
-/*Solution 1 using String*/
+*/
+
+/*Solution using String*/
 /*
 class Solution {
     public int[] evenOddBit(int n) {
@@ -39,7 +62,7 @@ class Solution {
 }
 */
 
-/*Solution 2*/
+/*Best Solution using Integer.bitCount*/
 /*
 public int[] evenOddBit(int n) {
     return new int[] {Integer.bitCount(n & 0b0101010101), 

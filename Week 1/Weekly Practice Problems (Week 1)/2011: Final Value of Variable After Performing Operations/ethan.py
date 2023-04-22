bee0795b88e45e1b696285ab75854866357bc9d6
hello there <98435853+ethanbleier@users.noreply.github.com>
2023-04-22 07:03:20 -0700

    ethan (#161)

---
+++ Week 1/Weekly Practice Problems (Week 1)/2011: Final Value of Variable After Performing Operations/ethan.py
@@ -0,0 +1,10 @@
+class Solution: # "X++","X++"
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        x = 0
+        for i in operations:
+            if i == "X++" or i == "++X":
+                x += 1
+            else:
+                x -= 1
+            
+        return x
